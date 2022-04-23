a9fbecd1bd80f75c1e533145fe18ca4779642da3
chintan0513 <chintantripathi1310@gmail.com>
2022-04-24 02:27:37 +0530

    connection done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express= require("express");
 const diseases = require("./data/diseases");
 const dotenv = require('dotenv')
+const connectDB = require("./config/db")
 
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get('/', (req,res) => {
     res.send('API is running')
