f4b88e38c4f2ef7458e1cb8ff66a81e92b74771d
chintan0513 <chintantripathi1310@gmail.com>
2022-04-23 23:25:06 +0530

    initial setup

---
+++ backend/data/diseases.js
@@ -0,0 +1,110 @@
+const diseases = [
+    {
+      _id: "1",
+      title: "Cough",
+      content:
+        "A cough is supposed to protect you. It gets out stuff that doesn't belong in your lungs and windpipe, like inhaled dirt or food.",
+      category: "Cold",
+      medicine: "Paracetamol",
+      image: "https://i.ibb.co/9rqhL59/paracetamol.jpg",
+    },
+    {
+      _id: "2",
+      title: "Blood Pressure",
+      content: "Blood pressure is the measure of the force of blood pushing against blood vessel walls.The heart pumps blood into blood vessels, which carry the blood throughout the body.",
+      category: "Blood Disease",
+      medicine: "Accupril, Aceon, Adalat, Serpina",
+      image: "https://i.ibb.co/Z1HtSHV/accupril.jpg",
+    },
+    {
+      _id: "3",
+      title: "Heart Attack",
+      content: "A heart attack occurs when one or more of your coronary arteries becomes blocked. Over time, a buildup of fatty deposits, including cholesterol, form substances called plaques, which can narrow the arteries (atherosclerosis).",
+      category: "Heart Diseases",
+      medicine: "Accupril",
+      image: "https://i.ibb.co/Z1HtSHV/accupril.jpg",
+    },
+    {
+      _id: "4",
+      title: "Fever",
+      content:
+        "A fever is a temporary increase in your body temperature, often due to an illness. Having a fever is a sign that something out of the ordinary is going on in your body.",
+      category: "Normal Disease",
+      medicine: "Paracetamol",
+      image: "https://i.ibb.co/9rqhL59/paracetamol.jpg",
+    },
+    {
+      _id: "5",
+      title: "Headache",
+      content:
+        "Headache is the most common form of pain. It's a major reason people miss days at work or school or visit the doctor.",
+      category: "pain",
+      medicine: "Acetaminophen, Aspirin, Imigran",
+      image: "https://i.ibb.co/2ZvktSc/acetaminophen.jpg",
+    },
+    {
+      _id: "6",
+      title: "Diabetes",
+      content:
+        "Diabetes is a disease that occurs when your blood glucose, also called blood sugar, is too high.",
+      category: "Serios Disease",
+      medicine: "Actos",
+      image: "https://i.ibb.co/fFh5XdN/actos.jpg",
+    },
+    {
+      _id: "7",
+      title: "Asthma Attack",
+      content:
+        "During an asthma attack, also called an asthma exacerbation, the airways become swollen and inflamed. The muscles around the airways contract and the airways produce extra mucus, causing the breathing (bronchial) tubes to narrow.",
+      category: "Serios Disease",
+      medicine: "Albuterol, Beclate, Seroflo",
+      image: "https://i.ibb.co/cL0FRgF/albuterol.jpg",
+    },
+    {
+      _id: "8",
+      title: "Allergies",
+      content:
+        "Allergies are your body's reaction to a normally harmless substance such as pollen, molds, animal dander, latex, certain foods and insect stings.-",
+      category: "Minor Disease",
+      medicine: "Allegra",
+      image: "https://i.ibb.co/F5BMvhL/allegra.jpg",
+    },
+    {
+      _id: "9",
+      title: "Fear",
+      content:
+        "Fear is an intensely unpleasant emotion in response to perceiving or recognizing a danger or threat. ",
+      category: "Tension",
+      medicine: "Buspirone",
+      image: "https://i.ibb.co/2KZhj4S/buspirone.jpg",
+    },
+    {
+      _id: "10",
+      title: "Dehydration",
+      content:
+        "Dehydration occurs when you use or lose more fluid than you take in, and your body doesn't have enough water and other fluids to carry out its normal functions.",
+      category: "Major Disease",
+      medicine: "Duphalac",
+      image: "https://i.ibb.co/TW5BS3M/duphalac.webp",
+    },
+    {
+      _id: "11",
+      title: "Eye Itch",
+      content:
+        "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
+      category: "Eya Disease",
+      medicine: "Ketotifen",
+      image: "https://i.ibb.co/k0FwhKZ/ketotifen.jpg",
+    },
+    {
+      _id: "12",
+      title: "Depression",
+      content:
+        "A mental health disorder characterised by persistently depressed mood or loss of interest in activities, causing significant impairment in daily life.",
+      category: "Panic Disorder",
+      medicine: "Zoloft",
+      image: "https://i.ibb.co/QbcT3Xv/zoloft.jpg",
+    },
+  ];
+  
+  module.exports = diseases;
---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express= require("express");
+const diseases = require("./data/diseases");
+const dotenv = require('dotenv')
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req,res) => {
+    res.send('API is running')
+});
+
+app.get('/api/disease', (req,res) => {
+    res.send(diseases)
+});
+
+app.get('/api/disease/:id', (req,res) => {
+    const disease = diseases.find((n) => n._id === req.params.id);
+    res.send(disease);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`))
